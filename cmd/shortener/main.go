499dba45438b9ea50991f1ae43c41baf1a2585bb
Anton Kurin <anton.kurin@rustainless.com>
2026-07-06 16:49:22 +0300

    iter6 - added request/response logging middleware

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"antonovxx/go-musthave-shortener/internal/config"
 	"antonovxx/go-musthave-shortener/internal/handler"
+	"antonovxx/go-musthave-shortener/internal/middleware"
 	"antonovxx/go-musthave-shortener/internal/repository"
 	"antonovxx/go-musthave-shortener/internal/service"
 	"errors"
@@ -10,6 +11,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -19,17 +21,24 @@ func main() {
 		log.Fatal(err)
 	}
 
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Sync()
+
 	repos := repository.NewURLRepository()
 	shortener := service.NewShortenerService(repos, cfg.BaseURL)
 	urlHandler := handler.NewURLHandler(shortener)
 
 	r := chi.NewRouter()
+	r.Use(middleware.Logger(logger))
 	r.Post("/", urlHandler.HandleShortenURL)
 	r.Get("/{id}", urlHandler.HandleExpandURL)
 
-	log.Printf("Starting server on %s\n", cfg.ServerAddress)
+	logger.Info("Starting server", zap.String("address", cfg.ServerAddress))
 
 	if err := http.ListenAndServe(cfg.ServerAddress, r); !errors.Is(err, http.ErrServerClosed) {
-		log.Fatal(err)
+		logger.Fatal("server failed", zap.Error(err))
 	}
 }
